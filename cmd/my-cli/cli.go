11f178d06962c5a796bda48954e543b7b02330f9
Matthew Jones <jonesmat@usc.edu>
2020-08-10 00:35:31 -0700

    More stuff

--- cmd/my-cli/cli.go
+++ cmd/my-cli/cli.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"github.com/urfave/cli"
 )
 
 func main() {
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "Website Lookup"
+	app.Usage = "Query IP, CNAME, MX, Name Servers"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "mwj96.dev",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Look up Name Server",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					return err
+				}
+
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
