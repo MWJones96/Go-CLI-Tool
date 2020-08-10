5a4a279eb20509bf0418c0bf9f466cecc5801dfb
Matthew Jones <jonesmat@usc.edu>
2020-08-10 00:40:23 -0700

    IP done

--- cmd/my-cli/cli.go
+++ cmd/my-cli/cli.go
@@ -21,7 +21,7 @@ func main() {
 		},
 	}
 
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		{
 			Name:  "ns",
 			Usage: "Look up Name Server",
@@ -34,6 +34,23 @@ func main() {
 
 				for i := 0; i < len(ns); i++ {
 					fmt.Println(ns[i].Host)
+				}
+
+				return nil
+			},
+		},
+		{
+			Name:  "ip",
+			Usage: "Looks up host IP address",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				ip, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					return err
+				}
+
+				for i := 0; i < len(ip); i++ {
+					fmt.Println(ip[i])
 				}
 
 				return nil
